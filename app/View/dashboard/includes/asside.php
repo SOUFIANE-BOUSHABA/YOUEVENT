489659b6c9cd6c1f0b2ecf63815db2733b21cd48
souf139 <soufianboushaba12@gmail.com>
2023-12-26 15:27:34 +0100

    this commit is for creating the frontend for login and regester pages

---
+++ app/View/dashboard/includes/asside.php
@@ -0,0 +1,20 @@
+<aside class="col-md-2 bg-dark text-light p-4 aside">
+                
+                <ul class="list-unstyled">
+                   
+                    <li><a href="#">dashboard</a></li>
+                    <li><a href="utilisteur.php">users</a></li>
+                    <li><a href="request.php">requests</a></li>
+                    <li><a href="hotel.php">hotels</a></li>
+                  
+                    <li><a href="hotel.php">hotels</a></li>
+                    <li><a href="romms.php">romms</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="requestresponsable.php">request</a></li>
+                 
+                    <li><a href="hotel.php">hotel</a></li>
+                    <li><a href="romms.php">romms</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                  
+                </ul>
+ </aside>
