9b2960667e99bda60895c170326660c5e1208e62
elkhailihamza <elkhailihamza331@gmail.com>
2023-12-27 11:33:18 +0100

    changed login icon on header

--- app/View/includes/head.php
+++ app/View/includes/head.php
@@ -10,6 +10,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"
+    type='text/css'>
 
   <link rel="stylesheet" href="./assets/css/bootstrap.min.css" type="text/css">
   <link rel="stylesheet" href="./assets/css/font-awesome.min.css" type="text/css">
--- app/View/includes/header.php
+++ app/View/includes/header.php
@@ -4,33 +4,37 @@
         <div class="loader"></div>
     </div>
     <!-- Header Section Begin -->
-<header class="header-section">
-    <div class="container">
-        <div class="logo">
-            <a href="./index.html">
-                <img src="./assets/img/logo.png" alt="">
-            </a>
+    <header class="header-section">
+        <div class="container">
+            <div class="logo">
+                <a href="./index.html">
+                    <img src="./assets/img/logo.png" alt="">
+                </a>
+            </div>
+            <div class="nav-menu">
+                <nav class="mainmenu mobile-menu">
+                    <ul>
+                        <li class="active"><a href="./index.html">Home</a></li>
+                        <li><a href="./about-us.html">About</a></li>
+                        <li><a href="./speaker.html">Speakers</a>
+                            <ul class="dropdown">
+                                <li><a href="#">Jayden</a></li>
+                                <li><a href="#">Sara</a></li>
+                                <li><a href="#">Emma</a></li>
+                                <li><a href="#">Harriet</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="./schedule.html">Schedule</a></li>
+                        <li><a href="./blog.html">Blog</a></li>
+                        <li><a href="./contact.html">Contacts</a></li>
+                    </ul>
+                </nav>
+                <a href="#" class="primary-btn top-btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                        viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M15 3h6v18h-6M10 17l5-5-5-5M13.8 12H3" />
+                    </svg> Login</a>
+            </div>
+            <div id="mobile-menu-wrap"></div>
         </div>
-        <div class="nav-menu">
-            <nav class="mainmenu mobile-menu">
-                <ul>
-                    <li class="active"><a href="./index.html">Home</a></li>
-                    <li><a href="./about-us.html">About</a></li>
-                    <li><a href="./speaker.html">Speakers</a>
-                        <ul class="dropdown">
-                            <li><a href="#">Jayden</a></li>
-                            <li><a href="#">Sara</a></li>
-                            <li><a href="#">Emma</a></li>
-                            <li><a href="#">Harriet</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="./schedule.html">Schedule</a></li>
-                    <li><a href="./blog.html">Blog</a></li>
-                    <li><a href="./contact.html">Contacts</a></li>
-                </ul>
-            </nav>
-            <a href="#" class="primary-btn top-btn"><i class="fa fa-ticket"></i> Ticket</a>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-    </div>
-</header>
+    </header>
