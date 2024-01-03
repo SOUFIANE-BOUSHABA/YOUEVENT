1298490e78bc885c5bf0d40db4d722ea0be27925
souf139 <soufianboushaba12@gmail.com>
2024-01-03 17:26:43 +0100

    fix errors

--- app/Controller/logoutController.php
+++ app/Controller/logoutController.php
@@ -4,6 +4,12 @@ class LogoutController {
     static function logoutUser() {
         if (isset($_POST['logout'])) {
             session_destroy();
+       
         }
+    }
+
+    public function test(){
+        session_destroy();
+        include '../app/View/login.php';
     }
 }
