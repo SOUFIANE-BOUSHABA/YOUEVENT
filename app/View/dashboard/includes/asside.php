99423df74f0e9b94caa286eb3b6689b5129edcd5
souf139 <soufianboushaba12@gmail.com>
2024-01-03 11:34:48 +0100

    update

--- app/View/dashboard/includes/asside.php
+++ app/View/dashboard/includes/asside.php
@@ -1,8 +1,8 @@
 <aside class="col-md-2 bg-dark text-light p-4 aside">
                 <ul class="list-unstyled">
                    
-                    <li> <i class="fa-solid fa-chart-simple"></i> <a href="?uri=admin/showStatistics">Dashboard</a></li>
-                    <li><a href="?uri=admin/getAllUser">Users</a></li>
+                    <li> <i class="fa-solid fa-chart-simple"></i> <a href="/YOUEVENT/admin/showStatistics">Dashboard</a></li>
+                    <li><a href="/YOUEVENT/admin/getAllUser">Users</a></li>
                    
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Reservations</a></li>
